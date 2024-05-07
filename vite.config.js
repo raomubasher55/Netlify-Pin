dbeb90a910ed9bbcb877d757305c660f0fe53bca
raomubasher55 <raomubasher5555@gmail.com>
2024-05-07 11:51:31 -0700

    Add more file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: '0.0.0.0',
-    cors: true,
-  },
+  plugins: [react()]
 });
